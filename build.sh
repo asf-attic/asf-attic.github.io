9d1485f5ad3c4d5722db3863a6ec96b98431288e
Hervé Boutemy <hboutemy@apache.org>
2018-05-05 17:27:26 +0200

    generate docs/flagged files from comments in projects/*.xml

--- build.sh
+++ build.sh
@@ -3,3 +3,14 @@
 # Script to build the Attic site from xdocs/ sources to docs/ html output
 
 ant -Danakia.check.date=false
+
+\rm -rf docs/flagged
+mkdir docs/flagged
+for retired in `grep 'retired flag:' xdocs/projects/*.xml | awk '{ print $3 }'`
+do
+  for d in $retired $retired.us $retired.eu
+  do
+    mkdir docs/flagged/$d.apache.org
+    touch docs/flagged/$d.apache.org/$d.apache.org
+  done
+done
